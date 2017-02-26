424ac81cdf62acb9bc6c7371af5bd378420cf46f
Jordan Buckmaster <buckmast@purdue.edu>
2017-02-25 21:24:46 -0600

    Cleaning up and documenting code.

--- src/SmileDetector.java
+++ src/SmileDetector.java
@@ -22,7 +22,7 @@ public class SmileDetector {
 	//depending on whether there is a person smiling or not in the image.
 	public static void main(String[] args) 
 	{
-		File[] files = new File("C:\\Users\\Jordan\\git\\SmileDetector").listFiles(new FileFilter() {
+		File[] files = new File("C:\\Users\\Jordan\\Desktop\\database 2\\lfw").listFiles(new FileFilter() {
 
 			@Override
 			public boolean accept(File arg0) {
@@ -30,12 +30,17 @@ public class SmileDetector {
 			}
 		});
 
+		//Load OpenCV and create classifiers using OpenCV's pre-trained classifiers.
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		CascadeClassifier faceClassifier = new CascadeClassifier(FACE_CASCADE);
 		CascadeClassifier smileClassifier = new CascadeClassifier(SMILE_CASCADE);
 		CascadeClassifier eyeClassifier = new CascadeClassifier(EYE_CASCADE);
 		
+		
+		//perform detection on every jpg in directory
 		for(File file : files) {
+			
+			//put image onto a mat and detect face
 			Mat image = Imgcodecs.imread(file.getName());
 			MatOfRect faceDetection = new MatOfRect();
 			faceClassifier.detectMultiScale(image, faceDetection);
@@ -43,16 +48,19 @@ public class SmileDetector {
 
 			if(faceDetection.toArray().length != 0) {
 				MatOfRect smileDetection = new MatOfRect();
-				MatOfRect eyeDetection = new MatOfRect();
+				//MatOfRect eyeDetection = new MatOfRect();
 				for (Rect rect : faceDetection.toArray()) {
+					//draw rectangle around face
 					Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
 							new Scalar(0, 0, 255));
-					Mat eyeRoi = new Mat(image, rect);
-					int xOffset = rect.x;
-					int yOffset = rect.y;
 					
-					Rect mouthRoi = new Rect(rect.x, rect.y+(rect.height/2), rect.width, rect.height/2);
-					yOffset = mouthRoi.y;
+					//Mat eyeRoi = new Mat(image, rect);
+					
+					int xOffset = rect.x;
+					Rect mouthRect = new Rect(rect.x, rect.y+(rect.height/2), rect.width, rect.height/2);
+					int yOffset = mouthRect.y;
+					
+					//Detect eyes
 					/*eyeClassifier.detectMultiScale(eyeRoi, eyeDetection);
 					for(Rect eyeRect : eyeDetection.toArray()) {
 						Imgproc.rectangle(image, new Point(xOffset + eyeRect.x, yOffset + eyeRect.y), new Point(xOffset + eyeRect.x + eyeRect.width, yOffset + eyeRect.y + eyeRect.height),
@@ -63,24 +71,27 @@ public class SmileDetector {
 						}	
 					}*/
 					
-					
-					Mat smileRoi = new Mat(image, mouthRoi);	
+					//detect smiles in face region
+					Mat smileRoi = new Mat(image, mouthRect);	
 					smileClassifier.detectMultiScale(smileRoi, smileDetection);
 					for (Rect smileRect : smileDetection.toArray()) {
+						//draw rectangle around smile
 						Imgproc.rectangle(image, new Point(xOffset + smileRect.x, yOffset + smileRect.y), new Point(xOffset + smileRect.x + smileRect.width, yOffset + smileRect.y + smileRect.height),
 								new Scalar(0, 255, 0));
 					}
+					
+					//add to negative or positive folder depending on result
 					if(smileDetection.toArray().length == 0) {
-						System.out.println("No smile found!");
+						System.out.println("Negative sample");
 						newFileName = "negative/"+file.getName();
 					} else {
-						System.out.println("Positive sample!");
+						System.out.println("Positive sample");
 						newFileName = "positive/"+file.getName();
 						break;
 					}
 				}
 			} else {
-				System.out.println("No face found!");
+				System.out.println("Negative sample");
 				newFileName = "negative/"+file.getName();
 			}
 
